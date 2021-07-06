60d3d84fd33a481e146525c6a005b1ee239df8f5
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-07-06 21:49:57 +0300

    Added test case 2 and 3

--- test-appium/src/test/java/CovidFormTest.java
+++ test-appium/src/test/java/CovidFormTest.java
@@ -1,10 +1,16 @@
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,6 +19,8 @@ import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 import java.util.Locale;
 
 public class CovidFormTest {
@@ -20,20 +28,23 @@ public class CovidFormTest {
     public AndroidDriver<MobileElement> driver;
     public WebDriverWait wait;
 
+    // Action for swipes/scroll
+    TouchAction action;
+
     // Elements for testing
-    By titleBy = By.id("com.example.cs_458covidformapplication:id/title");
     By nameInputBy = By.id("com.example.cs_458covidformapplication:id/name_input");
     By lastNameInputBy = By.id("com.example.cs_458covidformapplication:id/last_name_input");
-    By dateTextBy = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TextView[2]");
 
-    By datePicker = By.id("com.example.cs_458covidformapplication:id/date_of_birth");
-    By citySelectionBy = By.id("com.example.cs_458covidformapplication:id/citySelection");
-    By genderTextBy = By.id("com.example.cs_458covidformapplication:id/genderText");
+    By citySelectionBy = By.id("com.example.cs_458covidformapplication:id/city_selection");
+
     By maleRadioBy = By.id("com.example.cs_458covidformapplication:id/genderMale");
     By femaleRadioBy = By.id("com.example.cs_458covidformapplication:id/genderFemale");
     By otherRadioBy = By.id("com.example.cs_458covidformapplication:id/genderOther");
     By vaccineSelectionBy = By.id("com.example.cs_458covidformapplication:id/vaccine_type_selection");
     By sideEffectsInputBy = By.id("com.example.cs_458covidformapplication:id/side_effects_input");
+
+    By submitButtonBy = By.id("com.example.cs_458covidformapplication:id/submitButton");
+    By resultMessageBy = By.id("com.example.cs_458covidformapplication:id/resultMessage");
 
 
     @BeforeMethod
@@ -51,8 +62,7 @@ public class CovidFormTest {
 
         driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
         wait = new WebDriverWait(driver, 10);
-
-
+        action = new TouchAction(driver);
     }
 
     @Test(enabled = true)
@@ -65,7 +75,7 @@ public class CovidFormTest {
 
     }
 
-    @Test(enabled = true, description = "At most one radio button (initially zero) for a radio button group should be checked at any time.")
+    @Test(enabled = false, description = "At most one radio button (initially zero) for a radio button group should be checked at any time.")
     public void radioButtonTest() throws InterruptedException {
         Reporter.log("[Test-Case 1] - At most one radio button (initially zero) for a radio button group should be checked at any time.", true);
         // Check that initially no radio buttons are selected
@@ -79,8 +89,6 @@ public class CovidFormTest {
         Assert.assertEquals(wait.until(ExpectedConditions.visibilityOfElementLocated(otherRadioBy)).getAttribute("checked"),
                 "false",
                 "[Test-Case 1.1] Other should not be checked");
-
-
 
 
         // Check for cases where only the male radio button should be checked
@@ -148,10 +156,8 @@ public class CovidFormTest {
                 "[Test-Case 1.2] Other should not be checked");
 
 
-
-
         // Check for cases where only the female radio button should be checked
-        Reporter.log("[Test-Case 1.3] Female radio button should be the only radio button checked when female radio button is clicked last",true);
+        Reporter.log("[Test-Case 1.3] Female radio button should be the only radio button checked when female radio button is clicked last", true);
         // female clicked directly
         wait.until(ExpectedConditions.visibilityOfElementLocated(femaleRadioBy)).click();
         Assert.assertEquals(wait.until(ExpectedConditions.visibilityOfElementLocated(maleRadioBy)).getAttribute("checked"),
@@ -213,7 +219,6 @@ public class CovidFormTest {
         Assert.assertEquals(wait.until(ExpectedConditions.visibilityOfElementLocated(otherRadioBy)).getAttribute("checked"),
                 "false",
                 "[Test-Case 1.3] Other should not be checked");
-
 
 
         // Check for cases where only the other radio button should be checked
@@ -284,8 +289,179 @@ public class CovidFormTest {
 
     }
 
+    @Test(enabled = true, description = "On valid form submission a validity message should be displayed.")
+    public void submissionSuccessTest() throws InterruptedException {
+        Reporter.log("[Test-Case 2] A message of validity should be shown accordingly on form submission", true);
+
+        // Successful submission
+        Reporter.log("[Test-Case 2.1] An approval message should be shown with valid form submission", true);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(nameInputBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(nameInputBy)).sendKeys("Mehmet");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(lastNameInputBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(lastNameInputBy)).sendKeys("Özdemir");
+
+        driver.hideKeyboard();
+        scrollDown();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(citySelectionBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(citySelectionBy)).sendKeys("Ankara");
+
+        driver.hideKeyboard();
+        scrollDown();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(maleRadioBy)).click();
+
+        scrollDown();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy)).sendKeys("Pfizer-BionTech");
+
+        driver.hideKeyboard();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(submitButtonBy)).click();
+
+        scrollDown();
+        Assert.assertEquals(wait.until(ExpectedConditions.visibilityOfElementLocated(resultMessageBy)).getText(),
+                "Form saved successfully",
+                "Correct valid submission message was not shown");
+
+        // Unsuccesful submission
+        Reporter.log("[Test-Case 2.2] An invalid submission message should be shown with invalid form submission", true);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy)).clear();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(submitButtonBy)).click();
+
+        scrollDown();
+        Assert.assertEquals(wait.until(ExpectedConditions.visibilityOfElementLocated(resultMessageBy)).getText(),
+                "Please check the information you provided",
+                "Correct invalid submission message was not show");
+
+    }
+
+    @Test(enabled = false, description = "City and Vaccine Type should only accept input from a predefined list and clear itself on unrecognized input")
+    public void autoCompleteInputTest() throws InterruptedException {
+        Reporter.log("[Test-Case 3] City and Vaccine Type should only accept input from a predefined list", true);
+
+        Reporter.log("[Test-Case 3.1] Both City and Vaccine Type are valid", true);
+
+        scrollDown();
+        scrollDown();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(citySelectionBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(citySelectionBy)).sendKeys("Ankara");
+
+        driver.hideKeyboard();
+        scrollDown();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy)).sendKeys("Pfizer-BionTech");
+
+        driver.hideKeyboard();
+        scrollDown();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(sideEffectsInputBy)).click();
+
+        driver.hideKeyboard();
+        scrollDown();
+        Assert.assertNotEquals(wait.until((ExpectedConditions.visibilityOfElementLocated(citySelectionBy))).getText(),
+                "City",
+                "City selection should not be empty");
+        driver.hideKeyboard();
+        Assert.assertNotEquals(wait.until((ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy))).getText(),
+                "Vaccine Type",
+                "Vaccine selection should not be empty");
+        driver.hideKeyboard();
+
+        Reporter.log("[Test-Case 3.2] City is invalid and Vaccine Type is valid", true);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(citySelectionBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(citySelectionBy)).sendKeys("Kastamonu");
+
+        driver.hideKeyboard();
+        scrollDown();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy)).sendKeys("Pfizer-BionTech");
+
+        driver.hideKeyboard();
+        scrollDown();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(sideEffectsInputBy)).click();
+
+        driver.hideKeyboard();
+        scrollDown();
+        Assert.assertEquals(wait.until((ExpectedConditions.visibilityOfElementLocated(citySelectionBy))).getText(),
+                "City",
+                "City selection should be empty");
+        driver.hideKeyboard();
+        Assert.assertNotEquals(wait.until((ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy))).getText(),
+                "Vaccine Type",
+                "Vaccine selection should not be empty");
+        driver.hideKeyboard();
+
+        Reporter.log("[Test-Case 3.3] City is valid and Vaccine Type is invalid", true);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(citySelectionBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(citySelectionBy)).sendKeys("İzmir");
+
+        driver.hideKeyboard();
+        scrollDown();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy)).sendKeys("Supervax");
+
+        driver.hideKeyboard();
+        scrollDown();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(sideEffectsInputBy)).click();
+
+        driver.hideKeyboard();
+        scrollDown();
+        Assert.assertNotEquals(wait.until((ExpectedConditions.visibilityOfElementLocated(citySelectionBy))).getText(),
+                "City",
+                "City selection should not be empty");
+        driver.hideKeyboard();
+        Assert.assertEquals(wait.until((ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy))).getText(),
+                "Vaccine Type",
+                "Vaccine selection should be empty");
+        driver.hideKeyboard();
+
+        Reporter.log("[Test-Case 3.4] City and Vaccine Type is invalid", true);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(citySelectionBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(citySelectionBy)).sendKeys("Kastamonu");
+
+        driver.hideKeyboard();
+        scrollDown();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy)).sendKeys("SuperVax");
+
+        driver.hideKeyboard();
+        scrollDown();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(sideEffectsInputBy)).click();
+
+        driver.hideKeyboard();
+        scrollDown();
+        Assert.assertEquals(wait.until((ExpectedConditions.visibilityOfElementLocated(citySelectionBy))).getText(),
+                "City",
+                "City selection should be empty");
+        driver.hideKeyboard();
+        Assert.assertEquals(wait.until((ExpectedConditions.visibilityOfElementLocated(vaccineSelectionBy))).getText(),
+                "Vaccine Type",
+                "Vaccine selection should be empty");
+        driver.hideKeyboard();
+
+    }
+
     @AfterMethod
     public void teardown() {
         driver.quit();
+    }
+
+    private void scrollDown() {
+        action
+                .press(PointOption.point(0, 600))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(500)))
+                .moveTo(PointOption.point(0, 100))
+                .release().perform();
+    }
+
+    private void scrollUp() {
+        action
+                .press(PointOption.point(0, 100))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(500)))
+                .moveTo(PointOption.point(0, 600))
+                .release().perform();
     }
 }
